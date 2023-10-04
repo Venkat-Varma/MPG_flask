17bf12af0721adb28fbf10acaf3de872c692436f
Venkat-Varma <137096741+Venkat-Varma@users.noreply.github.com>
2023-10-04 18:09:13 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pickle
+
+mpg_df = pd.read_csv("Auto MPG Reg.csv")
+mpg_df.horsepower = mpg_df.horsepower.fillna(mpg_df.horsepower.median())
+
+y = mpg_df.mpg
+X = mpg_df[["cylinders","displacement","horsepower","weight","acceleration","modelyear","origin"]]
+
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(X,y)
+reg.score(X,y)
+
+pickle.dump(reg,open("model1.pkl","wb"))
+model = pickle.load(open("model1.pkl","rb"))
